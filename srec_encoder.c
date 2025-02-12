86fc3e24b742cc44e410198d3b0b29f7ab3ac91b
Amalaviya3030 <aryanmalaviya708@gmail.com>
2025-02-12 17:23:28 -0500

    completed the function to generate records in srec format

--- srec_encoder.c
+++ srec_encoder.c
@@ -7,4 +7,21 @@ void encodeSREC(FILE* in, FILE* out) {
     int count, checksum;
 
     fprintf(out, "S00E0000555454414D2D41524F5241D1\n"); // "UTTAM-ARORA" in ASCII-Hex
+
+    while ((count = fread(buffer, 1, 16, in)) > 0) {
+        checksum = count + (address >> 8) + (address & 0xFF);
+
+        fprintf(out, "S1%02X%04X", count + 3, address);
+        for (int i = 0; i < count; i++) {
+            fprintf(out, "%02X", buffer[i]);
+            checksum += buffer[i];
+        }
+
+        checksum = (~checksum) & 0xFF;
+        fprintf(out, "%02X\n", checksum);
+        address += 16;
+    }
+
+    fprintf(out, "S503%04XFA\n", address / 16);
+    fprintf(out, "S9030000FC\n");
 }
