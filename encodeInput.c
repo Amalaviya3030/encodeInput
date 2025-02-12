e814f99ec92408e4406bc174a42a9bed09a087da
Amalaviya3030 <aryanmalaviya708@gmail.com>
2025-02-12 14:46:42 -0500

    added command line argument for parsing files

--- encodeInput.c
+++ encodeInput.c
@@ -1,20 +1,28 @@
-// encodeInput.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "encodeInput.h"
 
-#include <iostream>
+int main(int argc, char* argv[]) {
+    char* inputFile = NULL, * outputFile = NULL;
+    int srecFormat = 0;
 
-int main()
-{
-    
-}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    for (int i = 1; i < argc; i++) {
+        if (strncmp(argv[i], "-i", 2) == 0) {
+            inputFile = argv[i] + 2;
+        }
+        else if (strncmp(argv[i], "-o", 2) == 0) {
+            outputFile = argv[i] + 2;
+        }
+        else if (strcmp(argv[i], "-srec") == 0) {
+            srecFormat = 1;
+        }
+        else if (strcmp(argv[i], "-h") == 0) {
+            printf("Usage: encodeInput -i<inputfile> -o<outputfile> [-srec] [-h]\n");
+            return 0;
+        }
+        else {
+            printf("Invalid option: %s\n", argv[i]);
+            return 1;
+        }
+    }
