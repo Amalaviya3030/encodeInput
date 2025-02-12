3b01ea70c5a2da35b8f4251ff83ad7490e9f6945
Amalaviya3030 <aryanmalaviya708@gmail.com>
2025-02-12 17:02:39 -0500

    implemented encodeSREC function

--- srec_encoder.c
+++ srec_encoder.c
@@ -1,6 +1,10 @@
-#include <iostream>
+#include <stdio.h>
+#include "../inc/encodeInput.h"
 
-int main()
-{
+void encodeSREC(FILE* in, FILE* out) {
+    unsigned char buffer[16];
+    int address = 0x0000;
+    int count, checksum;
 
+    fprintf(out, "S00E0000555454414D2D41524F5241D1\n"); // "UTTAM-ARORA" in ASCII-Hex
 }
