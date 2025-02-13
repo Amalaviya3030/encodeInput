7c4af14bdc0b70420b493ca00880af3700aaeb93
Amalaviya3030 <aryanmalaviya708@gmail.com>
2025-02-12 20:50:34 -0500

    added header comment in header file

--- encodeInput.h
+++ encodeInput.h
@@ -1,7 +1,18 @@
+/*
+ * FILE: encodeInput.h
+ * PROJECT: A02 - System Programming
+ * PROGRAMMERS: Anchita Kakria, Uttam Arora, Aryan Malaviya
+ * FIRST VERSION: 12 feb 2025
+ * DESCRIPTION: This is the header file containing the function protoypes for
+                encoding function binary files into srec and asm format. THese functions will change
+                binary data into ASCII format.
+ */
+
+
 #ifndef ENCODE_INPUT_H
 #define ENCODE_INPUT_H
 
 void encodeSREC(FILE* in, FILE* out);
-void encodeASM(FILE* in, FILE * out);
+void encodeASM(FILE* in, FILE* out);
 
 #endif
